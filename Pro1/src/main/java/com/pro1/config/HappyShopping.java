1a3056ca319a179d923d1d91971b0040292a0bf9
Surya <rsurya037@gmail.com>
2018-06-18 01:07:38 +0530

    first commit

---
+++ Pro1/src/main/java/com/pro1/config/HappyShopping.java
@@ -0,0 +1,16 @@
+package com.pro1.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HappyShopping {
+public static void main(String[] args) {
+	List l=new ArrayList();
+	l.add(1);
+	l.add('a');
+	l.add("hi");
+	
+	System.out.println(l);
+		
+}
+}
---
+++ Pro1/src/main/java/com/pro1/config/Pro1Application.java
@@ -0,0 +1,12 @@
+package com.pro1.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro1Application.class, args);
+	}
+}
